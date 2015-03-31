bf2939b50ef36629efa21446acf6dac1aca804e1
Alex Y. Chan <ayb.chan@gmail.com>
2015-03-31 15:03:22 +0100

    alex adrian conflict test part 2

--- web/navbar.php
+++ web/navbar.php
@@ -1,12 +1,25 @@
 <?php 
 
-echo '<div>
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="register.php">Register</a></li>
-				<li><a href="news.asp">Events</a></li>
-				<li><a href="contact.asp">My Events</a></li>
-				<li><a href="about.asp">Account</a></li>
-			</ul>
-			</div>' 
+echo '
+
+			<head>
+				<style>
+					body { background: url("banner.jpg");
+						    background-repeat: no-repeat;
+							 background-size: 600px
+
+							}
+				</style>
+			</head>
+			
+			<div class="twelve columns nav mobile_hidden" >
+				<ul class="menu">
+					<li><a href="index.php" class="top-link active">Home</a></li>
+					<li><a href="register.php" class="top-link active">Register</a></li>
+					<li><a href="register.php" class="top-link active">Events</a></li>
+					<li><a href="register.php" class="top-link active">My Events</a></li>
+					<li><a href="register.php" class="top-link active"">Account</a></li>
+					<li><a href="register.php" class="top-link active"">Map</a></li>
+				</ul>
+			</div>'
 ?>
