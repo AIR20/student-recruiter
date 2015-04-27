fea2f40b8f9d0758a35350f0d49aa947ba1dcd17
Rihan Yang <sgryang3@liv.ac.uk>
2015-04-27 23:01:00 +0100

    Cancel show dropdown menu accroding to user roles

--- views/shared/navbar.php
+++ views/shared/navbar.php
@@ -25,7 +25,6 @@
 					<a href = "#">Calendar</a>
 				</li>
 				
-				<?php if (isset($user) && $user->isTeacher()): ?>
 				<li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Teacher<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
@@ -35,9 +34,7 @@
             <li><a href="<?php echo $app->urlFor('teacher_add_student');?>">Add students</a></li>
           </ul>
 				</li>
-				<?php endif; ?>
 
-				<?php if (isset($user) && $user->isStaff()): ?>
 				<li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Staff<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
@@ -45,9 +42,7 @@
 						<li><a href="<?php echo $app->urlFor('create_event');?>">Propose new event</a></li>
 					</ul>
 				</li>
-				<?php endif; ?>
 				
-				<?php if (isset($user) && $user->isAdmin()): ?>
 				<li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Administrator<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
@@ -59,7 +54,6 @@
 					<li><a href="<?php echo $app->urlFor('home');?>">Add new room</a></li>
           </ul>
         </li>
-        		<?php endif; ?>
 			</ul>
       
       <ul class="nav navbar-nav navbar-right">
