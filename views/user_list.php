dafdd22ecb68f9c4c32256a677a57423f6df4386
Alex Y. Chan <ayb.chan@gmail.com>
2015-04-27 14:14:29 +0100

    Display list of users in tables, tabs for each role

--- views/user_list.php
+++ views/user_list.php
@@ -5,25 +5,46 @@
     <?php require 'shared/navbar.php';?>
 		<?php $numEvents = count($events) ?>
 		<div class="container">
-			<div class="bs-docs-section">
-			<div class="row">
-			<div class="main col-md-12">
-
-				<h1 class="page-header">List of users</h1>
-				<div class="well col-md-8 col-md-offset-2">
-					<?php foreach($students as $student) : ?>
-						<div class="panel panel-primary">
-							<div class="panel-heading">
-								<h3 class="panel-title"><?php echo $student>id;?><h3>
-							</div>		
-							<div class="panel-body">
-								<p><?php echo $student->user_id?></p>
-								</br>
-							</div>
-						</div>
-					<?php  endforeach; ?>
-				</div>
-			</div>
+			<ul class="nav nav-tabs">
+ 				<li class="active"><a href="#Students" data-toggle="tab">Students</a></li>
+  				<li><a href="#Teachers" data-toggle="tab">Teachers</a></li>
+  				<li><a href="#Staff" data-toggle="tab">Staff</a></li>
+  			</ul>
+		
+			<div id="myTabContent" class="tab-content">
+	  			<div class="tab-pane fade active in" id="Students">
+	   				<table class="table table-striped table-hover ">
+					  <thead>
+					    <tr>
+					      <th>UserID</th>
+					      <th>SchoolID</th>
+					      <th>TeacherID</th>
+					      <th>Address1</th>
+					    </tr>
+					  </thead>
+					  <tbody>
+					    
+						<?php foreach($students as $student) : ?>
+						<tr>
+					      <td><?php echo $student->user_id; ?></td>
+					      <td><?php echo $student->school_id; ?></td>
+					      <td><?php echo $student->teacher_id; ?></td>
+					      <td><?php echo $student->address_line1; ?></td>
+					    </tr>
+						<?php  endforeach; ?>
+					  </tbody>
+					 </table>						
+					</div>
+	  			</div>
+	  			
+	  			<div class="tab-pane fade" id="Teachers">
+	    			
+	  			</div>
+			
+				<div class="tab-pane fade" id="Staff">
+	    			
+	  			</div>
+			
 			</div>
 		</div>
   </body>
