9019a7b895e0955ba2b30b0352de20acb1e7db5a
Rihan Yang <sgryang3@liv.ac.uk>
2015-04-28 10:56:58 +0100

    Add teacher test

---
+++ test/models/TeacherTest.php
@@ -0,0 +1,29 @@
+<?php
+class TeacherTest extends PHPUnit_Framework_TestCase {
+
+	public function testCreateTeacher() {
+		$t = new Teacher();
+		$t->firstname = "Wikc";
+		$t->lastname = "Dominik";
+		$t->hashed_password = "123456";
+		$t->email = "scodfk@example.com";
+		$t->gender = 0;
+		$t->dob = '1999-09-21';
+		$t->school_id = 1;
+		$t->phone = '07422 133456';
+
+		$this->assertTrue($t->save(), "Cannot create teacher");
+		$this->assertInternalType("int", $t->id);
+		return $t->id;
+	}
+
+	/**
+	 * @depends testCreateTeacher
+	 */
+	public function testFindTeacher($id) {
+		$t = Teacher::getTeacherById($id);
+		$this->assertEquals(1, $st->school_id);
+		$this->assertEquals('07422 133456', $st->phone);
+	}
+
+}
