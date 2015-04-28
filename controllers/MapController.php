2a80848f20b567cc379e34725fda5b8970612c96
Rihan Yang <sgryang3@liv.ac.uk>
2015-04-28 18:15:08 +0100

    Remove duplicate header

--- controllers/MapController.php
+++ controllers/MapController.php
@@ -1,10 +1,11 @@
 <?php
 
 Class MapController extends BaseController {
-	
+
 	# GET /
 	function display(){
+		$this->loadMap();
 		$this->app->render('map.php', $this->data);
 	}
-	
+
 }
--- views/map.php
+++ views/map.php
@@ -2,24 +2,14 @@
 <!DOCTYPE html>
 <html>
 <?php require 'shared/head.php';?>
-	<head>
-		<meta charset=utf-8 />
-    <title>A simple map</title>
-    <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
-    <script src='https://api.tiles.mapbox.com/mapbox.js/v2.1.8/mapbox.js'></script>
-    <link href='https://api.tiles.mapbox.com/mapbox.js/v2.1.8/mapbox.css' rel='stylesheet' />
-	</head>	
-
-  <style>
-      body { margin:0; padding:0; }
-      #map { position:absolute; top:7%; bottom:0; width:100%; }
-  </style>
-
   <body>
     <?php require 'shared/navbar.php';?>
-
-    <div id='map'></div>
-      <script>
+	<div class="container">
+		<h1 class="page-header">Campus Map</h1>
+		<div id='map'></div>
+	</div>
+	<script type="text/javascript">
+		$('#map').height($(window).height() - $('nav').height() - $('footer').height());
         L.mapbox.accessToken = 'pk.eyJ1IjoiYXliY2hhbiIsImEiOiJaNlV1dlBVIn0.PtSa8Vmur7aYb0jxUfANgA';
         var map = L.mapbox.map('map', 'aybchan.lmmiljek');
         map.layer.openPopup();
@@ -28,9 +18,7 @@
             map.featureLayer.on('click', function(e) {
               map.panTo(e.layer.getLatLng());
         });
-
-
-      </script>
-    </div>
+	</script>
+	<?php require 'shared/footer.php' ?>
   </body>
 </html>
