4619d3d781fb42dae615819ee16f03aea08bd807
Alex Y. Chan <ayb.chan@gmail.com>
2015-04-28 09:34:58 +0100

    Menu for student and teacher registration links

--- views/shared/navbar.php
+++ views/shared/navbar.php
@@ -58,10 +58,13 @@
       
       <ul class="nav navbar-nav navbar-right">
 				<?php if(!isset($_SESSION['user'])): ?>
-				<li>
-					<a href="<?php echo $app->urlFor('student_register'); ?>">Register</a>
+				<li class="dropdown">
+		        	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Register<span class="caret"></span></a>
+        			<ul class="dropdown-menu" role="menu">
+            			<li><a href="<?php echo $app->urlFor('student_register');?>">Student</a></li>
+           				<li><a href="<?php echo $app->urlFor('teacher_register');?>">Teacher</a></li>
+					</ul>
 				</li>
-				
 				
 				<li>
 					<a href="<?php echo $app->urlFor('login'); ?>">Login</a>
