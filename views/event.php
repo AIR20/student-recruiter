fc468f9ea48c388323d632a033a01cd1a6c8cea2
Rihan Yang <sgryang3@liv.ac.uk>
2015-04-28 16:53:03 +0100

    Use a two-column layout for listing event

--- views/event.php
+++ views/event.php
@@ -3,36 +3,74 @@
 	<?php require 'shared/head.php';?>
   <body>
     <?php require 'shared/navbar.php';?>
-		<?php $numEvents = count($events) ?>
-		<div class="container">
-		
-			<div class="bs-docs-section">
-			<div class="row">
-			<div class="main col-md-12">
+	<div class="container">
+		<h1 class="page-header">Upcoming events</h1>
+		<?php require 'shared/notice.php'; ?>
+		<div class="row">
+			<div class="main col-sm-12">
+				<div class="sidebar col-sm-3">
 
-				<h1 class="page-header">Upcoming events</h1>
-				<?php require 'shared/notice.php'; ?>
-				<div class="well col-md-8 col-md-offset-2">
+				<div id="event-type">
+					<h3>Event Type</h3>
+					<ul class="nav nav-pills nav-stacked">
+						<li role="presentation" class="active"><a href="#">All</a></li>
+						<li role="presentation"><a href="#">Openday</a></li>
+						<li role="presentation"><a href="#">Day Trip</a></li>
+						<li role="presentation"><a href="#">Coming Soon</a></li>
+					</ul>
+				</div>
+
+				<div id="tags-filter">
+					<h3>Tags Filter</h3>
+					<ul class="nav nav-pills nav-stacked">
+						<li role="presentation" class="active"><a href="#"><i class="fa fa-times fa-lg fa-fw"></i> Music</a></li>
+						<li role="presentation"><a href="#"><i class="fa fa-circle-o fa-lg fa-fw"></i> Science</a></li>
+						<li role="presentation"><a href="#"><i class="fa fa-circle-o fa-lg fa-fw"></i> Hot tub</a></li>
+						<li role="presentation"><a href="#"><i class="fa fa-circle-o fa-lg fa-fw"></i> Sci-Fi</a></li>
+						<li role="presentation"><a href="#"><i class="fa fa-circle-o fa-lg fa-fw"></i> Bullshit</a></li>
+					</ul>
+				</div>
+
+				</div>
+				<div class="content col-sm-9">
 					<?php foreach($events as $event) : ?>
-						<div class="panel panel-primary">
+						<div class="panel panel-default">
 							<div class="panel-heading">
 								<h3 class="panel-title"><a href="<?php echo $app->urlFor('view_event', array('id' => $event->id)); ?>"><?php echo $event->title ?></a></h3>
-							</div>		
+							</div>
 							<div class="panel-body">
-							<h5><?php echo $event->getBuildingName() . ' - ' .  $event->getRoomName(); ?></h5>
-								<h5><?php echo date('g:ia', strtotime($event->start_time)) . ' - ' . date('g:ia', strtotime($event->end_time)) . ', ' . date('l jS F, Y', strtotime($event->start_time));?></h5>
+							<h5><i class="fa fa-building-o fa-fw"></i> <?php echo $event->getBuildingName() . ' - ' .  $event->getRoomName(); ?></h5>
+							<h5><i class="fa fa-clock-o fa-fw"></i> <?php echo date('g:ia', strtotime($event->start_time)) . ' - ' . date('g:ia', strtotime($event->end_time)) . ', ' . date('l jS F, Y', strtotime($event->start_time));?></h5>
 								<p><?php echo $event->description ?></p>
 								</br>
 								<div style="float:right">
-									<a href="<?php echo $app->urlFor('book_event', array('id' => $event->id)); ?>" class="btn btn-primary">Book</a> 
+									<a href="<?php echo $app->urlFor('book_event', array('id' => $event->id)); ?>" class="btn btn-primary"><i class="fa fa-thumb-tack fa-lg fa-fw"></i> Book</a>
 								</div>
 							</div>
 						</div>
-					<?php  endforeach; ?>
+					<?php endforeach; ?>
 				</div>
 			</div>
-			</div>
 		</div>
+	</div>
+	<?php require('shared/footer.php') ?>
+	<script type="text/javascript">
+		$("#event-type a").click(function() {
+			$('#event-type li').removeClass('active');
+			$(this).parent().addClass('active');
+		});
+
+		$("#tags-filter a").click(function() {
+			if ($(this).parent().hasClass('active')) {
+				$(this).parent().removeClass('active');
+				$(this).children('i').removeClass('fa-times');
+				$(this).children('i').addClass('fa-circle-o');
+			} else {
+				$(this).parent().addClass('active');
+				$(this).children('i').removeClass('fa-circle-o');
+				$(this).children('i').addClass('fa-times');
+			}
+		});
+	</script>
   </body>
 </html>
-
