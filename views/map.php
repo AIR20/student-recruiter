232e625656bb81c4c513138c3b1b4b117239694c
Rihan Yang <sgryang3@liv.ac.uk>
2015-04-29 11:48:10 +0100

    Adjust map height according to window size

--- views/map.php
+++ views/map.php
@@ -9,15 +9,13 @@
 		<div id='map'></div>
 	</div>
 	<script type="text/javascript">
-		$('#map').height($(window).height() - $('nav').height() - $('footer').height());
-        L.mapbox.accessToken = 'pk.eyJ1IjoiYXliY2hhbiIsImEiOiJaNlV1dlBVIn0.PtSa8Vmur7aYb0jxUfANgA';
-        var map = L.mapbox.map('map', 'aybchan.lmmiljek');
-        map.layer.openPopup();
-            map.addControl(L.mapbox.geocoderControl('mapbox.places'));
-            map.setView([53.405, -2.966], 15);
-            map.featureLayer.on('click', function(e) {
-              map.panTo(e.layer.getLatLng());
-        });
+		$('#map').height($(window).height() - 280);
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYXliY2hhbiIsImEiOiJaNlV1dlBVIn0.PtSa8Vmur7aYb0jxUfANgA';
+    var map = L.mapbox.map('map', 'aybchan.lmmiljek');
+    map.setView([53.4060706, -2.9661172], 25);
+    map.featureLayer.on('click', function(e) {
+      map.panTo(e.layer.getLatLng());
+    });
 	</script>
 	<?php require 'shared/footer.php' ?>
   </body>
