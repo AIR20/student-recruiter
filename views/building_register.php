9a51bd904a6e91fc3d696e1682896ecf58f4e50f
Alex Y. Chan <ayb.chan@gmail.com>
2015-04-30 06:10:42 +0100

    Improve add building form

--- views/building_register.php
+++ views/building_register.php
@@ -8,73 +8,79 @@
 	<div class="row">
 		<div class="main col-md-12">
 
-			<!-- page start -->
 			<h1 class="page-header">Add a Building</h1>
 			<?php require 'shared/notice.php'; ?>
 
-			<!--form -->
 			<div class="well col-md-8 col-md-offset-2">
 
 			<form action="<?php echo $app->urlFor('building_store'); ?>" method="post" class="form-horizontal">	            
 
-				<!--building name-->
 				<div class="form-group">
 					<label class="col-sm-2 control-label">
 						Building name:
 					</label>
-					<div class="col-sm-10">
-						<input class="form-control" type="text" name="name">
+					<div class="col-sm-8">
+						<input class="form-control" type="text" name="name" placeholder="Royal Liver Building">
 					</div>
 				</div>	
 
-				<div class="building-select">
-					<div id="small-map"></div>
-					<input id="lat" type="hidden" name="latitude">
-					<input id="lon" type="hidden" name="longitude">
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+						Building geolocate:
+					</label>
+					<div class="col-sm-10">
+						<div class="building-select">
+							<div id="small-map"></div>
+							<input id="lat" type="hidden" name="latitude">
+							<input id="lon" type="hidden" name="longitude">
+						</div>
+						<script type="text/javascript">
+							L.mapbox.accessToken = 'pk.eyJ1IjoiYXliY2hhbiIsImEiOiJaNlV1dlBVIn0.PtSa8Vmur7aYb0jxUfANgA';
+							var map = L.mapbox.map('small-map', 'aybchan.lmmiljek');
+							map.setView([53.4060706, -2.9661172], 16);
+							var marker;
+							map.on('click', function(e) {
+								if (marker) {
+									marker.setLatLng(e.latlng);
+								} else {
+									marker = L.marker(e.latlng, {
+											icon: L.mapbox.marker.icon({
+											'marker-size': 'large',
+											'marker-symbol': 'building',
+											'marker-color': '#158cba'
+										})
+									});
+									marker.addTo(map);
+								}
+								$("#lat").val(e.latlng.lat);
+								$("#lon").val(e.latlng.lng);
+							});
+							map.featureLayer.on('click', function(e) {
+								if (marker) {
+									marker.setLatLng(e.latlng);
+								} else {
+									marker = L.marker(e.latlng, {
+											icon: L.mapbox.marker.icon({
+											'marker-size': 'large',
+											'marker-symbol': 'building',
+											'marker-color': '#158cba'
+										})
+									});
+									marker.addTo(map);
+								}
+								$("#lat").val(e.latlng.lat);
+								$("#lon").val(e.latlng.lng);
+							});
+						</script>
+						<p><small>Click on the map to drop a marker where this building is. This will fetch longitude and latitude geolocation data.</small></p>
+					</div>
 				</div>
-				<script type="text/javascript">
-					L.mapbox.accessToken = 'pk.eyJ1IjoiYXliY2hhbiIsImEiOiJaNlV1dlBVIn0.PtSa8Vmur7aYb0jxUfANgA';
-					var map = L.mapbox.map('small-map', 'aybchan.lmmiljek');
-					map.setView([53.4060706, -2.9661172], 16);
-					var marker;
-					map.on('click', function(e) {
-						if (marker) {
-							marker.setLatLng(e.latlng);
-						} else {
-							marker = L.marker(e.latlng, {
-									icon: L.mapbox.marker.icon({
-									'marker-size': 'large',
-									'marker-symbol': 'building',
-									'marker-color': '#158cba'
-								})
-							});
-							marker.addTo(map);
-						}
-						$("#lat").val(e.latlng.lat);
-						$("#lon").val(e.latlng.lng);
-					});
-					map.featureLayer.on('click', function(e) {
-						if (marker) {
-							marker.setLatLng(e.latlng);
-						} else {
-							marker = L.marker(e.latlng, {
-									icon: L.mapbox.marker.icon({
-									'marker-size': 'large',
-									'marker-symbol': 'building',
-									'marker-color': '#158cba'
-								})
-							});
-							marker.addTo(map);
-						}
-						$("#lat").val(e.latlng.lat);
-						$("#lon").val(e.latlng.lng);
-					});
-				</script>
 
-			<!--submit form-->
-			<input class="col-sm-offset-2 btn btn-primary" type="submit">
+			<div class="col-sm-offset-2">
+				<button type="reset" class="btn btn-default">Reset</button>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
 		</form>
-	<!-- end of registration form -->
 	</div>
 <!-- page end -->
 
