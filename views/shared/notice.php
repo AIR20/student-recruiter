036908db3d57d2ecc4c85648a47a0765201b555c
Rihan Yang <sgryang3@liv.ac.uk>
2015-05-01 10:18:00 +0100

    Correct notice title

--- views/shared/notice.php
+++ views/shared/notice.php
@@ -1,7 +1,7 @@
 <?php if (isset($flash['error'])): ?>
 <div class="alert alert-dismissible alert-danger">
 	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Warning</h4>
+	<h4>Error</h4>
 	<p><?php echo $flash['error']; ?></p>
 </div>
 <?php endif; ?>
@@ -17,7 +17,7 @@
 <?php if (isset($flash['success'])): ?>
 <div class="alert alert-dismissible alert-success">
 	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Warning</h4>
+	<h4>Well done</h4>
 	<p><?php echo $flash['success']; ?></p>
 </div>
 <?php endif; ?>
@@ -25,7 +25,7 @@
 <?php if (isset($flash['info'])): ?>
 <div class="alert alert-dismissible alert-info">
 	<button type="button" class="close" data-dismiss="alert">&times;</button>
-	<h4>Warning</h4>
+	<h4>Notice</h4>
 	<p><?php echo $flash['info']; ?></p>
 </div>
 <?php endif; ?>
