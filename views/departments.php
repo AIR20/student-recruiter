35011aea4dc8c774c61080563ad1538d81b5b5cb
Alex Y. Chan <ayb.chan@gmail.com>
2015-05-02 12:37:30 +0100

    Fix formatting in department list page

--- views/departments.php
+++ views/departments.php
@@ -3,35 +3,41 @@
 	<?php require 'shared/head.php';?>
   <body>
     <?php require 'shared/navbar.php';?>
-	<div class="container">
-		<h1 class="page-header">Department list</h1>
-		<?php require 'shared/notice.php'; ?>
+   <div class="container">
+	<h1 class="page-header">Department list</h1>
+	<?php require 'shared/notice.php'; ?>
 		<div class="row">
-			<div class="main col-sm-11 col-md-offset-1">
-				
-				<div class="content col-sm-5">
-					<?php foreach($departments as $department) : ?>
-					<div class="panel panel-default">
-						<div class="panel-heading"><?php echo $department->name; ?></div>
-	  					<div class="panel-body">
-								<?php echo Building::getBuildingById($department->building_id)->name; ?> 
-								</br>
-								<?php echo $department->address_line1; ?>
-								</br>
-								<?php echo $department->address_line2; ?>
+		<?php foreach($departments as $department) : ?>
+		<div class="col-sm-4">
+			<div class="student panel panel-default">
+				<div class="panel-heading"><?php echo $department->name; ?></div>
+				<div class="panel-body">
+					<div class="row">
+					<div class="col-md-8">
+					<?php echo Building::getBuildingById($department->building_id)->name; ?> 
+						</br>
+						<?php echo $department->address_line1; ?>
+						</br>
+						<?php if(isset($department->address_line2)):
+							echo $department->address_line2; ?>
 							</br>
-	    					<?php foreach($staffs as $staff) :?>
-	    						<?php if($staff->department_id==$department->id) : ?>
-	    							<?php echo $staff->id; ?>
-	    							<?php echo $staff->firstname. ' ' . $staff->name; ?>
-	    						<?php endif; ?>
-	    					<?php endforeach; ?>
-	    				</div>	
+						<?php endif; ?>
+						<?php echo $department->address_line3 . ' ' . $department->postcode; ?>
+						</br>
+						</br>
+						Staff:
+						</br>
+			    		<?php foreach($staffs as $staff) :?>
+			    		<?php if($staff->department_id==$department->id) : ?>
+			    			<?php echo $staff->firstname. ' ' . $staff->lastname; ?>
+			    			</br><?php endif; ?>
+			    		<?php endforeach; ?>
 					</div>
-					<?php endforeach; ?>
+					</div>
 				</div>
 			</div>
 		</div>
+		<?php endforeach; ?>
 	</div>
 	<?php require 'shared/footer.php'; ?>
   </body>
