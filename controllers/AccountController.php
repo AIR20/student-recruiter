0b0ce257a9bab55fa97cc5e7d80edb8a192bacb1
Rihan Yang <sgryang3@liv.ac.uk>
2015-05-02 13:04:30 +0100

    Bug fixed for account detail page
    Now every user can see their account detail page.

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -5,8 +5,11 @@ class AccountController extends BaseController {
 
 	# GET /authenticate
 	function display(){
-		if(isset($this->data['user'])){
-			$this->data['events'] = $this->data['user']->getEventList();
+		if(isset($this->user)){
+
+			$this->data['events'] = array();
+			if ($this->user->isStudent())
+				$this->data['events'] = $this->data['user']->getEventList();
 			$this->app->render('account_detail.php', $this->data);
 		} else {
 			$this->app->flash('error', 'Please login first');
