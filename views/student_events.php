a7051aa5835879753b39910cb6b50236ce3d12ea
Alex Y. Chan <ayb.chan@gmail.com>
2015-05-06 11:49:01 +0100

    Fix student's 'my events' for past and upcoming events

--- views/student_events.php
+++ views/student_events.php
@@ -4,30 +4,15 @@ CTYPE html>
   <body>
     <?php require 'shared/navbar.php';?>
   <div class="container">
-    <h1 class="page-header">Your upcoming events</h1>
+    <h1 class="page-header">Your events</h1>
     <?php require 'shared/notice.php'; ?>
     <div class="row">
       <div class="main col-sm-12">
 
-        <div class="sidebar col-sm-3">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							<h3 class="panel-title">Personal details</h3>
-						</div>
-						<div class="panel-body">
-							<p><big><i class="fa fa-user"></i> Name:  <?php echo $user->firstname;?> <?php echo $user->lastname;?></big> </p>
-							<p><big><i class="fa fa-envelope"></i> Email:  <?php echo $user->email;?> </big> </p>
-              <p><big><i class="fa fa-calendar"></i> DOB: <?php echo $user->dob;?> </big> </p>
-              <p><big><i class="fa fa-venus-mars"></i> Gender:  <?php if($user->gender == 0) echo "Male"; else echo "Female";?> </big> </p>
-              <p><big><i class="fa fa-user-plus"></i> Registered:  <?php echo $user->registered_at?> </big> </p>
-						</div>
-					</div>
-				</div>
-
-				<div class="content col-sm-9">
+				<div class="content col-sm-11 col-sm-offset-1">
           <div class="panel panel-default">
             <div class="panel-heading">
-              <h3 class="panel-title">Your upcoming events</h3>
+              <h3 class="panel-title">Upcoming events</h3>
             </div>
             <div class="panel-body">
 							<table class="table table-striped table-hover ">
@@ -44,7 +29,7 @@ CTYPE html>
 									</tr>
 								</thead>
 								<tbody>
-									<?php $i=1; foreach ($events as $event): ?>
+									<?php date_default_timezone_set("Europe/London");  $i=1; foreach ($events as $event): if($event->end_time > date("Y-m-d h:i:s", time())): ?>
 									<tr>
 										<td><?php echo $i++;?></td>
 										<td><?php echo $event->type;?></td>
@@ -55,12 +40,45 @@ CTYPE html>
 										<td><?php echo $event->getRoomName();?></td>
 										<td><a href="<?php echo $app->urlFor('unbook_event', array('id' => $event->id)); ?>" class="btn btn-danger btn-xs"><i class="fa fa-close fa-lg fa-fw"></i>Cancel Booking</a> </td>
 									</tr>
-		              <?php endforeach; ?>
+		              <?php endif; endforeach; ?>
+								</tbody>
+							</table>
+            </div>
+          </div>
+ 			  </div>
+		
+				<div class="content col-sm-11 col-sm-offset-1">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Past events</h3>
+            </div>
+            <div class="panel-body">
+							<table class="table table-striped table-hover ">
+							  <thead>
+							    <tr>			
+										<th>#</th>
+										<th>Type</th>
+										<th>Title</th>
+										<th>Date</th>
+										<th>Feedback</a>
+									</tr>
+								</thead>
+								<tbody>
+									<?php date_default_timezone_set("Europe/London"); $i=1; foreach ($events as $event): if($event->end_time < date("Y-m-d h:i:s", time())): ?>
+									<tr>
+										<td><?php echo $i++;?></td>
+										<td><?php echo $event->type;?></td>
+										<td><a href="<?php echo $app->urlFor('view_event', array('id' => $event->id)); ?>"><?php echo $event->title;?></a></td>
+										<td><?php echo date('l jS F', strtotime($event->start_time));?></td>
+										<td><a href="<?php echo $app->urlFor('give_feedback', array('id' => $event->id)); ?>" class="btn btn-success btn-xs"><i class="fa fa-comments fa-lg fa-fw"></i>Give feedback</a> </td>
+									</tr>
+		              <?php endif; endforeach; ?>
 								</tbody>
 							</table>
             </div>
           </div>
       </div>
+
 		</div>
 		<?php include 'shared/footer.php' ?>
   </body>
